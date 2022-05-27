b5fc90d0340ec15fd061a3f4769b7dbb93f7de2d
Skyyyr <emuswgluvr@gmail.com>
2022-05-26 17:01:21 -0700

    Finished py

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,11 @@
+import re
 # Don't forget to run your tests!
 
 def is_character_match(string1, string2):
-	pass
-	# Your code here!
+	array_of_letters_left = [x for x in string1.lower()]
+	array_of_letters_right =[x for x in string2.lower()]
+	for element in array_of_letters_left:
+		if not element in array_of_letters_right:
+			return False
+		array_of_letters_right.remove(element)
+	return True
